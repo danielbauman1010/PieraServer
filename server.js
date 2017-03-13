9543f68ace504c4250682a30785fb9eb64f9fd77
daniel bauman <danielbauman2000@daniels-MacBook-Air-2.local>
2017-03-13 11:57:56 -0400

    server set for testing

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express=require('express')
+var bodyParser=require('body-parser')
 var app = express()
-app.listen(3000)
+
+app.use(bodyParser.json())
+
+
 app.get('/',function(req,res) {
   console.log('request made.')
   res.end("This works.")
 })
+
+app.post('/createuser', function(req,res) {
+  console.log(req.body)
+  res.end("User recieved.")
+})
+
+
+app.listen(3000)
