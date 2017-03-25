3633ecfdd2a255085877539a1e6dfa12dbeaf82d
danielbauman1010 <danielbauman1010@gmail.com>
2017-03-25 05:01:59 +0000

    trying to get ssl to work

--- server.js
+++ server.js
@@ -5,22 +5,26 @@ var fs = require('fs')
 var http = require('http')
 var https = require('https')
 var server = express()
-
+//var path = require('path')
 server.use(bodyParser.json())
-
+server.use(express.static('public'))
 
 //SSL:
 //openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days XXX
 var pass = 'Piera123'
-var sslOptions = {
+/*var sslOptions = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem'),
   passphrase: pass
-};
+};*/
 
 server.get('/',function(req,res) {
   console.log('request made.')
   res.end("This works.")
+})
+
+server.get('/.well-known/acme-challenge/0Vus4jhT628fhBP8TpU8AWdYxBp7ohcbM_-GRFhT6rc', function(req,res) {
+	res.end("Valid response. Very trustworthy. Definitely real.");
 })
 
 server.post('/createuser', function(req,res) {
@@ -30,5 +34,5 @@ server.post('/createuser', function(req,res) {
 })
 
 
-http.createServer(server).listen(3000)
-https.createServer(sslOptions, server).listen(8000)
+http.createServer(server).listen(80)
+//https.createServer(sslOptions, server).listen(443)
